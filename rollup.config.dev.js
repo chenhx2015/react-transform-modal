97e58140577cc793f9022f23d578a10bb27dce3e
chenhuaxiang <chenhx9211@163.com>
2020-04-28 00:02:29 +0800

    build

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,32 +1,32 @@
-import json from '@rollup/plugin-json'
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import replace from '@rollup/plugin-replace';
+import json from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import replace from "@rollup/plugin-replace";
 
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
-import React from 'react'
-import ReactDOM from 'react-dom'
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import React from "react";
+import ReactDOM from "react-dom";
 
 export default {
-  input: 'examples/index.js' ,
-  output:  { file: 'examples/bundle.js', format: 'umd' , name: 'reactPortal' },
+  input: "docs/index.js",
+  output: { file: "docs/bundle.js", format: "umd", name: "reactPortal" },
   plugins: [
     json(),
     resolve(),
-    babel({ exclude: 'node_modules/**' }),
+    babel({ exclude: "node_modules/**" }),
     commonjs({
       namedExports: {
         react: Object.keys(React),
-        'react-dom': Object.keys(ReactDOM)
-      }
+        "react-dom": Object.keys(ReactDOM),
+      },
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      "process.env.NODE_ENV": JSON.stringify("development"),
     }),
 
-    serve('examples/'),
+    serve("docs/"),
     livereload(),
   ],
 };
